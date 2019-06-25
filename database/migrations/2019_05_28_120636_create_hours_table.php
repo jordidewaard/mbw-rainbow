fe9d13209711b20fd845346521cb26687ffdef5d
Alaa Darkashli <darkashli@icloud.com>
2019-06-25 20:11:05 +0200

    Migration (pivot table) configuring

--- database/migrations/2019_05_28_120636_create_hours_table.php
+++ database/migrations/2019_05_28_120636_create_hours_table.php
@@ -15,14 +15,7 @@ class CreateHoursTable extends Migration
     {
         Schema::create('hours', function (Blueprint $table) {
 
-            $table->integer('project_id')->unsigned();
-            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
-
-            $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-
-            $table->primary(['project_id', 'user_id']);
-
+            $table->increments('id');
             $table->date('date');
             $table->integer('hours');
             $table->mediumText('Description');
@@ -38,9 +31,6 @@ class CreateHoursTable extends Migration
      */
     public function down()
     {
-        Schema::table('hours', function (Blueprint $table) {
-            $table->dropForeign(['user_id', 'project_id']);
-            $table->dropColumn(['project_id', 'user_id']);
-        });
+        Schema::dropIfExists('hours');
     }
 }
