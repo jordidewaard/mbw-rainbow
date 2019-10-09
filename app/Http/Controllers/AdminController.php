d5c3eac23bddb688ea8d1b1c413909512273b71e
LAPTOP-4A79OJ3C\jordi <jordidewaard97@gmail.com>
2019-10-09 10:20:23 +0200

    Lijst met leraren weergeven mogelijk gemaakt

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class AdminController extends Controller
 {
@@ -13,6 +14,11 @@ class AdminController extends Controller
     public function admin()
     {
 		return view('teachers.teachers');
+    }
+
+    public function showteachers() {
+        $teachers = User::where('role', 'A')->paginate(10);
+        return view('teachers.teachers')->with('teachers', $teachers);
     }
 
 }
