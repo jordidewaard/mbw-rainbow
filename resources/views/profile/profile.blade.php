94df572a740dc29c834adea226d9c0ed7b0d782d
LAPTOP-4A79OJ3C\jordi <jordidewaard97@gmail.com>
2020-03-30 14:55:13 +0200

    Added home, login and logout function for profile

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -9,9 +9,23 @@
                         <svg class="card-img-top" xmlns="http://www.w3.org/2000/svg" width="240" height="240" viewBox="0 0 24 24"></svg>
                         <div class="card-body">
                             {{ Auth::user()->name }}
+                            @if (Route::has('login'))
+                                <div class="top-right links">
+                                    @auth
+                                        <a href="{{ url('/home') }}">Home</a>
+                                    @else
+                                        <a href="{{ route('login') }}">Login</a>
+
+                                        @if (Route::has('register'))
+                                            <a href="{{ route('register') }}">Register</a>
+                                        @endif
+                                    @endauth
+                                </div>
+                            @endif
                         </div>
                     </div>
             </div>
         </div>
     </div>
+
 @endsection
