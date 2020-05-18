dc32e85fabe249b7494d19d41d10901693ed7605
LAPTOP-4A79OJ3C\jordi <jordidewaard97@gmail.com>
2020-05-18 14:14:48 +0200

    Laravel title bugfix

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -6,8 +6,8 @@
 @extends('layouts.app')
 
 @section('content')
+    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
